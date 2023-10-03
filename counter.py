01f4468e65639fa9daae0bceb8f5638c88960f4e
Oscar Fuentes <oscar.afm96@gmail.com>
2023-10-03 16:41:01 -0600

    Simple tracking v1

--- counter.py
+++ counter.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from object_detection import ObjectDetection
+import math
 
 od = ObjectDetection()
 
@@ -14,14 +15,22 @@ with open('dnn_model\classes.txt', 'r') as f:
 np.random.seed(42)
 colors = np.random.randint(0, 255, size=(len(classes), 3), dtype='uint8').tolist()
 
-# Contador ID
+# Frame counter
 count = 0
 
-center_points = []
+cp_pre_frame = []
+tracking_objects = {}
+track_id = 0
 
 while True:
-  _, frame = cap.read()
+  ret, frame = cap.read()
+  count += 1
+  if not ret:
+    break
 
+  # Point current frame
+  cp_crnt_frame = []
+  
   (class_ids, scores, boxes) = od.detect(frame)
 
   cv2.line(frame, (450, 150), (1250, 350), (0, 0, 255), 2)
@@ -32,7 +41,7 @@ while True:
     cx = int((x + x + w) / 2.0)
     cy = int((y + y + h) / 2.0)
     
-    center_points.append((cx, cy))
+    cp_crnt_frame.append((cx, cy))
     
     class_id = class_ids[idx]
     confidence = scores[idx]
@@ -54,11 +63,37 @@ while True:
     # Dibuja el rectángulo alrededor del objeto
     cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
   
-  for pt in center_points:
+  if count <= 2:
+    for pt in cp_crnt_frame:
+      for pt2 in cp_pre_frame:
+        distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+        if distance < 20:
+          tracking_objects[track_id] = pt
+          track_id += 1
+  else:
+    tracking_objects_copy = tracking_objects.copy()
+    cp_crnt_frame_copy = cp_crnt_frame.copy()
+    for object_id, pt2 in tracking_objects_copy.items():
+      object_exists = False
+      for pt in cp_crnt_frame_copy:
+        distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+        if distance < 20:
+          tracking_objects[object_id] = pt
+          object_exists = True
+          if pt in cp_crnt_frame:
+            cp_crnt_frame.remove(pt)
+          continue
+      if not object_exists:
+        tracking_objects.pop(object_id)
+    for pt in cp_crnt_frame:
+      tracking_objects[track_id] = pt
+      track_id += 1
+  for object_id, pt in tracking_objects.items():
     cv2.circle(frame, pt, 2, (0, 255, 0), -1)
-
+    cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 255, 0), 2)
+    
   cv2.imshow("Frame", frame)
-  key = cv2.waitKey(1)
+  key = cv2.waitKey(0)
   if key == 27:  # Si se presiona la tecla 'Esc', termina el bucle
     break
 
