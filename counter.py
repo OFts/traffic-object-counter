6b2bc656156ae481184546e469a6d5f0318dd57f
Oscar Fuentes <oscar.afm96@gmail.com>
2023-10-03 22:27:56 -0600

    Counting objects

--- counter.py
+++ counter.py
@@ -15,12 +15,38 @@ with open('dnn_model\classes.txt', 'r') as f:
 np.random.seed(42)
 colors = np.random.randint(0, 255, size=(len(classes), 3), dtype='uint8').tolist()
 
+# Posición inicial para imprimir el texto
+x_count, y_count = 10, 30
+text_height = 25
+
 # Frame counter
 count = 0
 
 cp_pre_frame = []
 tracking_objects = {}
+crossing_count = {}
 track_id = 0
+
+# Limit line
+x1 = 450
+y1 = 150
+x2 = 1250
+y2 = 350
+a = x1 - x2
+b = y2 - y1
+c = (y1 * a) + (x1 * b)
+
+# Line cross detection function 
+def cross_line(pp, cp):
+  d_prev =pp[1] * a + pp[0] * b
+  d = cp[1] * a + cp[0] * b
+  #print(d)
+  # Determina si el centroide cruzó la línea
+  if d_prev <= c and d > c: # Going up
+      return True
+  if d_prev >= c and d < c: # Going down
+      return True
+  return False # Not crossing
 
 while True:
   ret, frame = cap.read()
@@ -33,7 +59,7 @@ while True:
   
   (class_ids, scores, boxes) = od.detect(frame)
 
-  cv2.line(frame, (450, 150), (1250, 350), (0, 0, 255), 2)
+  cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
   
   for idx, box in enumerate(boxes):
     (x, y, w, h) = box
@@ -41,10 +67,10 @@ while True:
     cx = int((x + x + w) / 2.0)
     cy = int((y + y + h) / 2.0)
     
-    cp_crnt_frame.append((cx, cy))
-    
     class_id = class_ids[idx]
     confidence = scores[idx]
+    
+    cp_crnt_frame.append((cx, cy, class_id))
 
     # Obtener color y etiqueta para esta clase
     color = colors[class_id]
@@ -62,6 +88,9 @@ while True:
 
     # Dibuja el rectángulo alrededor del objeto
     cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+    
+    # Dibuja el centroide
+    cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
   
   if count <= 2:
     for pt in cp_crnt_frame:
@@ -80,20 +109,49 @@ while True:
         if distance < 20:
           tracking_objects[object_id] = pt
           object_exists = True
+          #print(cross_line(pt2, pt))
+          if cross_line(pt2, pt):
+            #print("cross")
+            try:
+              crossing_count[pt[2]]
+            except:
+              crossing_count[pt[2]] = 1
+            else:
+              crossing_count[pt[2]] += 1
           if pt in cp_crnt_frame:
             cp_crnt_frame.remove(pt)
           continue
       if not object_exists:
         tracking_objects.pop(object_id)
     for pt in cp_crnt_frame:
-      tracking_objects[track_id] = pt
+      tracking_objects[track_id] = pt # New object
       track_id += 1
-  for object_id, pt in tracking_objects.items():
-    cv2.circle(frame, pt, 2, (0, 255, 0), -1)
-    cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 255, 0), 2)
+  
+  # Add object count to upper left corner
+  y_count = 30
+  for class_id, quantity in crossing_count.items():
+    text = f"{classes[class_id]}: {quantity}"
+    # Calcula el ancho del texto
+    (text_width, _), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
     
+    # Dibuja el rectángulo negro de fondo
+    cv2.rectangle(frame, (x_count, y_count - text_height), (x_count + text_width, y_count), (0, 0, 0), -1)
+    
+    # Dibuja el texto blanco
+    cv2.putText(frame, text, (x_count, y_count - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    y_count += 30  # Aumenta la posición en y para que los textos no se superpongan
+
+  
+  # for object_id, pt in tracking_objects.items():
+  #   cv2.circle(frame, pt[0:2], 20, (0, 255, 0), 2)
+  #   cv2.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 255, 0), 2)
+  
+  #print("Tracking objects")
+  #print(tracking_objects)
+  print(crossing_count)
+  
   cv2.imshow("Frame", frame)
-  key = cv2.waitKey(0)
+  key = cv2.waitKey(1)
   if key == 27:  # Si se presiona la tecla 'Esc', termina el bucle
     break
 
